255f8d72bb92b8d32540ca129f649841f657a3fa
jomi <mijo.kristo@outlook.com>
2019-02-25 10:14:36 +0000

    Adding files1123.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/dist/index.html"));
+app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('server started '+ port);
